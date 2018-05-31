81d016a8cec7df21e2fe171756bea29b237548e2
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-31 02:21:15 -0300

    se agrega formiula descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,12 @@
+window.cipher = {
 
-
-
-
-window.onload = function(){
-let textoUsuario = "";
-let offset = 0;
+  encode: (textoUsuario , offsetUsuario) => {
+   let textoCifrado= "";
+   let offset = 0;
 
 
 //se hace un for para recorrer el texto ingresado x el user
-function cipher() {
+
 
 for (i = 0; i < textoUsuario.length; i++){
 let codigoAscci = textoUsuario.toUpperCase().charCodeAt(i);
@@ -20,7 +18,25 @@ textoCifrado = textoCifrado + caracterCipher;
 }
 return textoCifrado
 //funcion concatena los caracteres cifrados y forma el nuevo mensaje
-}
- 
+  },
 
+  decode: (textoUsuario , offsetUsuario) => {
+    let textoCifrado= "";
+    let offset = 0;
+
+    for ( i = 0; i < textoUsuario.length; i++) {
+      let codigoAscci= textoUsuario.charCodeAt(i);
+      let codigoAscciDecipher = (codigoAscci + 65 - offset) % 26 + 65;
+      let caracterDecipher = String.fromCharCode (codigoAscciDecipher);
+      textoCifrado = textoCifrado + caracterDecipher;
+    }
+    return textoCifrado
+  }
 }
+
+
+
+
+
+
+ 

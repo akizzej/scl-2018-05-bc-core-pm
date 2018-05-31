2024ec0a83760a147858f8199c1aff97255a3f1b
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-31 19:00:23 -0300

    se guardan cambios en index.js funcion cifrar y se arregla orden de parametros de la funcion, se arregla orden de parametros funcion cipher siempre va primero texto, despues offset

--- cipher.js
+++ cipher.js
@@ -1,45 +1,40 @@
-
-let textoUsuario = "";
-let offset = 0 ;
-
-let textoCifrado ="";
-
-
-
-//se hace un for para recorrer el texto ingresado x el user
-function encode() {
-    textoUsuario = document.getElementById("text").value;
-    offsetUser = document.getElementById("OffSet").value; 
-    textoCifrado = document.getElementById("textoCifrado").innerHTML = textoCifrado;
-
-    for (i = 0; i <= textoUsuario.length; i++){
-let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
-let codigoCesarCipher = (mensaje- 65+ offset ) %26 + 65;
-let caracterCipher = String.fromCharCode(codigoCesarCipher);
-
-textoCifrado = textoCifrado+caracterCipher; 
-
-}
-return textoCifrado;
-
-//funcion concatena los caracteres cifrados y forma el nuevo mensaje
+window.cipher={
+    encode: (textoUsuario, offsetUser) => {
+        let offset = parseInt(offsetUser );
+        let textoCifrado ="";
 
 
 
-}
-function decode() {
+        //se hace un for para recorrer el texto ingresado x el user
 
-    for (i = 0; i < textoUsuario.length; i++){
-    let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
-    let codigoCesarCipher = (mensaje+65 - offset) %26 - 65;
-    let caracterCipher = String.fromCharCode(codigoAscciCipher);
-    
-    textoCifrado = textoCifrado + caracterCipher; 
-    
+        for (i = 0; i <= textoUsuario.length; i++){
+            let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
+            let codigoCesarCipher = (mensaje- 65+ offset ) %26 + 65;
+            let caracterCipher = String.fromCharCode(codigoCesarCipher);
+
+            textoCifrado += caracterCipher
+        }
+        return textoCifrado;
+    },
+
+    decode:(offsetUser, originalText) => {
+        
+        let offset = parseInt(offsetUser );
+        let textoCifrado ="";
+
+        for (i = 0; i < textoUsuario.length; i++){
+            let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
+            let codigoCesarCipher = (mensaje+65 - offset) %26 - 65;
+            let caracterCipher = String.fromCharCode(codigoAscciCipher);
+            
+            textoCifrado += caracterCipher; 
+        }
+        return textoCifrado
     }
-    return textoCifrado
-    //funcion concatena los caracteres cifrados y forma el nuevo mensaje
+        
+        //funcion concatena los caracteres cifrados y forma el nuevo mensaje
+}   
     
     
-    
-    }
+
+
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+function cifrar() {
+    const textoUsuario = document.getElementById("text").value;
+    const offset = document.getElementById("OffSet").value; 
+    document.getElementById("textoCifrado").innerHTML = window.cipher.encode(textoUsuario, offset);
 
+
+}
       
     
  

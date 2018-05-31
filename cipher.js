d78582dd379c45075e17a500293398db858cd90c
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-30 23:55:50 -0300

    funcion cipher ok

--- cipher.js
+++ cipher.js
@@ -1,9 +1,17 @@
-window.cipher = {
+//se declara una variable "textoUsuario" para guardar el texto que ingrese el usuario
+let textoUsuario = "";
+let offset = 0;
+let cifrado ="";
 
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+//se hace un for para recorrer el texto ingresado x el user
+function cipher (){
+for (i = 0; i < textoUsuario.length; i++)
+let codigoAscci = textoUsuario.toUpperCase().charCodeAt(i);
+let codigoAscciCipher = (codigoAscci- 65 + offset) %26 + 65;
+let caracterCipher = String.fromCharCode(codigoAscciCipher);
+
+cifrado = cifrado+ caracterCipher; 
+
 }
+//funcion concatena los caracteres cifrados y forma el nuevo mensaje
+return cifrado

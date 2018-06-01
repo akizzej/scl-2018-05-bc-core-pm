a120504acb5e55c782b79b5c0e92267167dd971f
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-06-01 16:07:29 -0300

    se soluciona problema en index.js, ahora funciona descifrado

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function cifrar() {
 function descifrar() {
   const textoCifrado = document.getElementById("text").value;
   const offset = document.getElementById("OffSet").value; 
-  document.getElementsById("textoCifrado").innerHTML  = window.cipher.decode(textoCifrado, offset);
+  document.getElementById("textoCifrado").innerHTML  = window.cipher.decode(textoCifrado, offset);
 
 
 }

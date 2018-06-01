28e50c22ea2db1d1f7bda96ca320a27517797ede
Jessica Alejandra Avila Vallejos <39282697+akizzej@users.noreply.github.com>
2018-06-01 15:23:05 -0400

    Rename docs/cipher.js to src/cipher.js

---
+++ cipher.js
@@ -0,0 +1,36 @@
+window.cipher={
+    encode: (textoUsuario, offsetUser) => {
+        let offset = parseInt(offsetUser );
+        let textoCifrado ="";
+        //se hace un for para recorrer el texto ingresado x el user
+
+        for (i = 0; i <= textoUsuario.length; i++){
+            let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
+            let codigoCesarCipher = (mensaje- 65+ offset ) %26 + 65;
+            let caracterCipher = String.fromCharCode(codigoCesarCipher);
+
+            textoCifrado += caracterCipher
+        }
+        return textoCifrado;
+    },
+
+    decode: (textoUsuario, offsetUser) => {
+        let offset = parseInt(offsetUser );
+        let textoCifrado ="";
+        //se hace un for para recorrer el texto ingresado x el user
+
+        for (i = 0; i <= textoUsuario.length; i++){
+            let mensaje = textoUsuario.toUpperCase().charCodeAt(i);
+            let codigoCesarCipher = (mensaje- 65- offset ) %26 + 65;
+            let caracterCipher = String.fromCharCode(codigoCesarCipher);
+
+            textoCifrado += caracterCipher
+        }
+        return textoCifrado;
+    }
+        
+        //funcion concatena los caracteres cifrados y forma el nuevo mensaje
+}   
+
+
+
